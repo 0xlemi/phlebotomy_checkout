65b680e3cc29235c32bdb689774147e7320fbe6d
Luis Espinosa <lem.espinosa.m@gmail.com>
2020-05-05 13:58:02 -0600

    make hamburgerbutton and responsive menu work

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -4,7 +4,13 @@
       <div class="flex items-center flex-shrink-0 text-white mr-6 lg:ml-8">
           <img class=" h-12 " src="https://www.phlebotomyusa.com/wp-content/uploads/2017/02/logo.jpg" alt="Phlebotomy USA">
       </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <!-- <div class="absolute inset-y-0 left-0 flex items-center sm:hidden"> -->
+      <div class="lg:hidden block flex w-auto">
+
+          <mobile-menu-button @click.native="toggleMenu" :active="hamburgerButton" ></mobile-menu-button>
+      </div>
+      <!-- Desktop Menu -->
+      <div class="hidden w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow">
               <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-gray-500 lg:ml-6 mr-8">
                   LOCATIONS
@@ -26,12 +32,52 @@
               </a>
           </div>
       </div>
+      <!-- End Desktop Menu -->
+      <!-- Mobile Menu -->
+      <div v-if="hamburgerButton" class="w-full ">
+          <div class="text-md mt-4">
+              <a href="#responsive-header" class="hover:bg-gray-200 rounded-lg block px-4 py-3 text-gray-700 hover:text-gray-800 ">
+                  LOCATIONS
+              </a>
+              <a href="#responsive-header" class="hover:bg-gray-200 rounded-lg block px-4 py-3 text-gray-700 hover:text-gray-800 ">
+                  VEINVIEWER
+              </a>
+              <a href="#responsive-header" class="hover:bg-gray-200 rounded-lg block px-4 py-3 text-gray-700 hover:text-gray-800 ">
+                  ABOUT US
+              </a>
+              <a href="#responsive-header" class="hover:bg-gray-200 rounded-lg block px-4 py-3 text-gray-700 hover:text-gray-800 ">
+                  CONTACT US
+              </a>
+              <a href="#responsive-header" class="hover:bg-gray-200 rounded-lg block px-4 py-3 text-gray-700 hover:text-gray-800 ">
+                  BLOG
+              </a>
+          </div>
+      </div>
+      <!-- End Mobile Menu -->
   </nav>
 </div>
 </template>
 
 <script>
+
+import DesktopMenu from '@/components/Layout/NavBar/DesktopMenu.vue';
+import MobileMenu from '@/components/Layout/NavBar/MobileMenu.vue';
+import MobileMenuButton from '@/components/Layout/NavBar/MobileMenuButton.vue';
+
 export default {
+  components: {
+    'mobile-menu-button': MobileMenuButton
+  },
+  methods: {
+    toggleMenu: function () {
+      this.hamburgerButton = !this.hamburgerButton;
+    }
+  },
+  data: function() {
+    return {
+      hamburgerButton: true
+    }
+  }
 
 }
 </script>
---
+++ src/components/Layout/NavBar/MobileMenuButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Mobile menu button-->
+  <button :class="[(active) ? 'bg-gray-600 text-white ' : 'text-gray-500']" class="inline-flex items-center justify-center p-2 focus:outline-none rounded-md  hover:text-white hover:bg-gray-500 transition duration-150 ease-in-out">
+    <!-- Icon when menu is closed. -->
+    <!-- Menu open: "hidden", Menu closed: "block" -->
+    <svg class="block h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+    </svg>
+    <!-- Icon when menu is open. -->
+    <!-- Menu open: "block", Menu closed: "hidden" -->
+    <svg class="hidden h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+    </svg>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['active']
+}
+</script>
+
+<style scoped>
+</style>
