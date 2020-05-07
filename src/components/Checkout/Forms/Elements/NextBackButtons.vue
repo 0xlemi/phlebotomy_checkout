899ea8c1937d7696ac0ee6139d53abb1926ca0bc
Luis Espinosa <lem.espinosa.m@gmail.com>
2020-05-06 20:29:39 -0600

    make mobile next and back button full width

--- src/components/Checkout/Forms/Elements/NextBackButtons.vue
+++ src/components/Checkout/Forms/Elements/NextBackButtons.vue
@@ -1,6 +1,8 @@
 <template>
-  <!-- Buttons -->
-  <div class="flex mt-10 mb-10">
+<div class="">
+
+  <!-- Desktop Buttons -->
+  <div class="hidden md:flex mt-10 mb-10">
     <div v-if="hasBackButton" class="flex-1">
       <button @click="back" type="button" class="underline focus:outline-none text-red-800 font-semibold hover:text-red-600 py-2 px-4">
          {{ backText }}
@@ -15,18 +17,40 @@
       </div>
     </div>
   </div>
-  <!-- End Buttons -->
-
-
+  <!-- End Desktop Buttons -->
+  <!-- Buttons Mobile -->
+  <div class="md:hidden mt-10 mb-10">
+    <div v-if="hasNextButton" class="flex mb-6" >
+      <button @click="onSubmit" type="submit" :class="{ 'opacity-50 cursor-not-allowed' : nextBlocked }"  class="w-full bg-red-800 focus:outline-none hover:bg-red-900 text-white font-bold py-4 rounded-lg">
+        <div class="flex justify-center">
+          <moon-loader class="" :loading="loading" color="#FFF5F5" size="25px"></moon-loader>
+          <p v-if="!loading">{{ nextText }}</p>
+        </div>
+      </button>
+    </div>
+    <div v-if="hasBackButton" class="">
+      <button @click="back" type="button" class="w-full border-2 border-red-200 hover:bg-red-200 hover:border-red-300 rounded-lg focus:outline-none text-red-800 font-semibold hover:text-red-900 py-2 px-4">
+        <div class="flex justify-center py-2">
+          <zondicon icon="cheveron-left" class="h-5 fill-current"/>
+          <p class="ml-2">{{ backText }}</p>
+        </div>
+      </button>
+    </div>
+  </div>
+  <!-- End Buttons Mobile -->
+</div>
 </template>
 
 <script>
 
 import { MoonLoader } from 'vue-spinner/dist/vue-spinner.min.js'
 
+import Zondicon from 'vue-zondicons';
+
 export default {
   components: {
-    'moon-loader' : MoonLoader
+    'moon-loader' : MoonLoader,
+    'zondicon': Zondicon
   },
   props: {
     hasBackButton: {
